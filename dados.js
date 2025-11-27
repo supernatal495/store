8fdc67b0875d1cbfa027bcbbf0963dee2ea237b6
supernatal495 <supernatal495@gmail.com>
2025-11-27 01:57:46 +0000

    comit 3

---
+++ dados.js
@@ -0,0 +1,10 @@
+const listaProdutos = [
+    { id: "1", titulo: "Big Panda", desc: "36Tiros Calibre 20", preco: "30", imagem: "1.jpeg", premium: true },
+    { id: "2", titulo: "Feliz Ano Novo", desc: "36Tiros Calibre18", preco: "25", imagem: "2.jpeg", premium: false },
+    { id: "3", titulo: "Festival Balls ou Sea Crab", desc: "48 Tiros Calibre 20", preco: "40", imagem: "3.jpg", premium: true },
+    { id: "4", titulo: "Gunfire", desc: "100Tiros Calbire 20", preco: "75", imagem: "4.png", premium: false },
+    { id: "5", titulo: "Happy Alfa", desc: "100 Tiros Calibre 16", preco: "40", imagem: "5.jpg", premium: false },
+    { id: "6", titulo: "Happy Alfa", desc: "49Tiros Calibre16", preco: "30", imagem: "6.png", premium: false },
+    { id: "7", titulo: "Happy Panda", desc: "36Tiros Calibre16", preco: "20", imagem: "7.png", premium: false },
+    { id: "8", titulo: "Pro", desc: "222 Tiros Calibre 20", preco: "175", imagem: "8.jpg", premium: true },
+];
