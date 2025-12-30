4ef612b58ede861c9f41dd372835a5d875a03e90
supernatal495 <supernatal495@gmail.com>
2025-12-30 01:30:46 +0000

    ack stock

--- dados.js
+++ dados.js
@@ -17,11 +17,11 @@ const listaProdutos = [
     { id: "1", titulo: "Big Panda", desc: "36Tiros Calibre 20", preco: "30", imagem: "1.png", premium: true, stock: true, poucasUnidades: false },
     { id: "2", titulo: "Feliz Ano Novo", desc: "36Tiros Calibre18", preco: "25", imagem: "2.png", premium: false, stock: true, poucasUnidades: false },
     { id: "3", titulo: "Mega Panda", desc: "100 Tiros Calibre 16", preco: "50", imagem: "10.png", premium: true, stock: false, poucasUnidades: false },
-    { id: "4", titulo: "Festival Balls ou Sea Crab", desc: "48 Tiros Calibre 20", preco: "40", imagem: "3.png", premium: true, stock: true, poucasUnidades: false },
+    { id: "4", titulo: "Festival Balls ou Sea Crab", desc: "48 Tiros Calibre 20", preco: "40", imagem: "3.png", premium: true, stock: true, poucasUnidades: true },
     { id: "5", titulo: "Gunfire", desc: "100Tiros Calbire 20", preco: "75", imagem: "4.png", premium: true, stock: true, poucasUnidades: false },
-    { id: "6", titulo: "Happy Alfa", desc: "100 Tiros Calibre 16", preco: "45", imagem: "5.png", premium: false, stock: true, poucasUnidades: true },
+    { id: "6", titulo: "Happy Alfa", desc: "100 Tiros Calibre 16", preco: "45", imagem: "5.png", premium: false, stock: false, poucasUnidades: false },
     { id: "7", titulo: "Happy Alfa", desc: "49Tiros Calibre16", preco: "30", imagem: "6.png", premium: false, stock: true, poucasUnidades: false },
-    { id: "8", titulo: "Happy Panda", desc: "36Tiros Calibre16", preco: "20", imagem: "7.png", premium: false, stock: true, poucasUnidades: false },
+    { id: "8", titulo: "Happy Panda", desc: "36Tiros Calibre16", preco: "20", imagem: "7.png", premium: false, stock: true, poucasUnidades: true },
     { id: "9", titulo: "Bateria Anti-Area", desc: "81 Disparos", preco: "10", imagem: "9.png", premium: false, stock: true, poucasUnidades: false },
     { id: "", titulo: "Pro", desc: "222 Tiros Calibre 20", preco: "175", imagem: "8.png", premium: true, stock: false, poucasUnidades: false },
 ];
@@ -30,7 +30,7 @@ const listaPetardos = [
     // --- BOMBINHAS / PETARDOS ---
     { id: "1", titulo: "Tracas", desc: "1 UNIDADE", preco: "1", imagem: "1.png", premium: false, stock: true, poucasUnidades: false },
     { id: "2", titulo: "25 Pk2", desc: "", preco: "6", imagem: "2.png", premium: false, stock: false, poucasUnidades: false },
-    { id: "3", titulo: "P2", desc: "Caixa P2 10 UNIDADES", preco: "10", imagem: "3.png", premium: false, stock: true, poucasUnidades: false },
+    { id: "3", titulo: "P2", desc: "Caixa P2 10 UNIDADES", preco: "10", imagem: "3.png", premium: false, stock: false, poucasUnidades: false },
     { id: "4", titulo: "Valencianetes", desc: "Valencianetes 20 UNIDADES", preco: "10", imagem: "4.png", premium: false, stock: true, poucasUnidades: true },
     { id: "5", titulo: "Tracas", desc: "Tracas 250 Picos", preco: "10", imagem: "5.png", premium: false, stock: true, poucasUnidades: false },
     { id: "6", titulo: "Foguete Viagem Lunar", desc: "144 UNIDADES", preco: "20", imagem: "6.png", premium: false, stock: true, poucasUnidades: false },
